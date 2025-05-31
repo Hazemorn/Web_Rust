9b4183e1e564171f272564e432c4f536f40a19a0
Mikita M <mikita@Mikitas-MacBook-Pro.local>
2025-05-31 14:55:55 +0300

    Modified module system. Added threads

---
+++ src/handler.rs
@@ -0,0 +1,37 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::{thread,time::Duration, fs};
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    /*println!(
+        "Request: {}",
+        String::from_utf8_lossy(&buffer[..])
+    )*/
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    }else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length {}\rn\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
